88227536c06a3e7ebb566423eb47fdb71de2655f
Lawson <lawsoncofield@gmail.com>
2019-12-14 11:35:09 -0800

    integer to roman

---
+++ JSFiles/Scratch.js
@@ -0,0 +1,8 @@
+function editsArray(myArr) {
+    myArr[0] = 5
+}
+
+testArr = [0, 1, 2, 3, 4]
+console.log(testArr)
+editsArray(testArr)
+console.log(testArr)
---
+++ JSFiles/TestClass2.js
@@ -0,0 +1,8 @@
+class Car {
+    contructor(brand, colour) {
+        this.brand = brand
+        this.colour = colour
+    }
+}
+
+var myCar = new Car("Subaru", "green")
